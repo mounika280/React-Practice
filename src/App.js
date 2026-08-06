39cda52560dbfe7139f236911b12ee0c4225ce3b
mounika280 <mounimarthi312@gmail.com>
2026-08-06 21:29:37 +0530

    react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { c } from './sample.js';
+import Day1 from './Day1.js';
+function App() {
+  return (
+    
+    <Day1/>
+  );
+}
+
+export default App;
---
+++ src/Day1.js
@@ -0,0 +1,9 @@
+function Day1() {
+  return (
+    <div>
+      <h1>Day 1</h1>
+    </div>
+  );
+}
+
+export default Day1;
